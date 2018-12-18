7539996434015b264803d589c53c2efc54274c0d
Golam Nabi <golamnabi411330@gmail.com>
2018-12-18 13:58:51 +0600

    create index file and find error in line 12

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php  
+
+$a = array('abc','ijk',25);
+class abc
+{
+	public function ijk($age){
+		echo "My name is Golam Nabi and my age is $age";
+	}
+}
+
+$c = new $a[0]();
+$c->ijk($a[2]);
+
+
+?>
